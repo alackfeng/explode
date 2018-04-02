bceac609011569b230ac4ed3b383fbcba0d37e70
alackfeng <fengyue7576199@163.com>
2018-04-02 17:57:20 +0800

    更新服务和隐私条款

--- js/components/utils/PrivacyService.js
+++ js/components/utils/PrivacyService.js
@@ -9,6 +9,77 @@ import { Button, Text, CheckBox, Overlay } from 'react-native-elements';
 import { SCREEN_WIDTH, SCREEN_HEIGHT, translate, locale } from '../../libs';
 
 
+class EmpowerTerm extends Component {
+
+
+  getItem = (items) => {
+
+    if(!items)
+      return null;
+
+    if(typeof items === 'object') {
+      const keys = Object.keys(items);
+      let itemsL = keys.map(key => {
+
+        if(typeof items[key] === 'string')
+          return <Text style={styles.empowerContentItem}>{ items[key] }</Text>;
+        else 
+          return <Text style={styles.empowerContentItem}>{ JSON.stringify(items[key]) }</Text>;;
+      })
+      return itemsL;
+    }
+    else {
+      return <Text style={styles.empowerContentItem}>{ JSON.stringify(items) }</Text>;
+    }
+  }
+
+  getSubTitle = () => {
+    const { term } = this.props;
+
+    if(!term || !term.subtitle) 
+      return null;
+
+    if(typeof term.subtitle === 'object') {
+      const keys = Object.keys(term.subtitle);
+      let subtitles = keys.map(key => {
+        if(typeof term.subtitle[key] === 'object') {
+          let subtitle = [];
+          subtitle.push(<Text style={styles.empowerContentSubTitle}>{ term.subtitle[key].title }</Text>);
+          subtitle.push(this.getItem(term.subtitle[key].items));
+          return subtitle;
+        }
+        else {
+          return <Text style={styles.empowerContentSubTitle}>{ term.subtitle[key] }</Text>;
+        }
+          
+      })
+      return subtitles;
+    } else {
+      return <Text style={styles.empowerContentSubTitle}>{ JSON.stringify(term.subtitle) }</Text>;
+    }
+    
+  }
+
+  getTitle = () => {
+    const { term } = this.props;
+
+    if(!term || !term.title)
+      return null;
+    return <Text style={styles.empowerContentTitle}>{ term.title }</Text>;
+  }
+
+  render() {
+
+    // 三级组件
+    return (
+      <View style={{marginBottom: 10}}>
+        { this.getTitle() }
+        { this.getSubTitle() }        
+      </View>
+    );
+  }
+}
+
 export class EmpowermentModel extends Component {
   onApproved = (agree) => {
     if (this.props.approved) {
@@ -20,6 +91,10 @@ export class EmpowermentModel extends Component {
     const { open } = this.props;
 
     if (open) {
+
+      const privacyservice = translate("privacyservice", locale);
+      const content = privacyservice.content;
+
       return (
         <Overlay
           isVisible={!!open}
@@ -30,31 +105,14 @@ export class EmpowermentModel extends Component {
           containerStyle={styles.overContainerStyle}
         >
           <View style={styles.empowerTitle}>
-            <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>TokenPii服务协议</Text>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', textAlign: 'center' }}>{ privacyservice.title }</Text>
           </View>
           <ScrollView style={styles.empowerContent}>
-            <Text>一、 关于本协议的确认与接纳
-
-1. 您理解本协议及有关协议适用于imToken及imToken上融识所自主开发和拥有的去中心化应用（简称“DApp”）（排除第三方开发的DApp）。
-2. 您下载imToken软件并创建或导入钱包，即视为您已经充分阅读并接受本协议全部条款，本协议立即生效，对双方具有约束力。
-3. 本协议可由融识随时更新，经修改的协议一经在imToken上公布，立即自动生效，恕不再另行通知。在融识公布修改协议条款后，如果您不接受修改后的条款，请立即停止使用imToken，您继续使用imToken将被视为接受修改后的协议。
-4. 如果您未满18周岁，或者是无民事行为能力人或限制民事行为能力人，请在父母或监护人指导下使用imToken。
-二、 定义
-
-1. imToken：指由融识基于以太坊单链系统（及将来陆续支持的其他区块链系统）开发的区块链钱包，包括其他为方便用户使用区块链系统而开发的辅助工具。
-2. 用户：
-2. 用户：
-
-（1）用户必须是具备完全民事行为能力的自然人；
-（2）若您为18周岁以下的未成年人使用imToken服务，需要在您父母或监护人的指导下使用imToken。无民事行为能力人使用imToken或限制民事行为能力人超过其民事权利或行为能力范围从事交易的，造成的一切后果，imToken有权要求您及您的父母或监护人负责。
-3. 能力测评问卷：指在您使用imToken之前（及在将来的使用过程中），需通过能力测评问卷以证明您了解区块链科技和知识，具备合理使用和管理去中心化钱包的基本能力。
-4. 创建或导入钱包：指您使用imToken，确认履行本协议并创建或导入钱包的过程。
-5. 钱包密码：指您在创建imToken钱包过程中，软件操作界面提示您填写的密码，该密码用于加密保护私钥。作为去中心化的应用，钱包密码不存储在您的这台移动设备或融识的服务器，一旦丢失你需要借助明文私钥或助记词重置新密码。
-6. 信息提示：imToken软件操作界面涉及的信息提示内容，建议用户按照相关步骤进行操作。
-7. 特定用户：指按照中国法律法规及政策规定必须要配合融识履行个人信息披露义务的用户。
-8. 私钥：由256位随机字符构成，是用户拥有并使用数字代币的核心。
-9. 公钥：由私钥借助密码学原理单向推导生成，并用以生
-            </Text>
+            <EmpowerTerm term={content.term0} />
+            <EmpowerTerm term={content.term1} />
+            <EmpowerTerm term={content.term2} />
+            <EmpowerTerm term={content.term3} />
+            <EmpowerTerm term={content.term4} />
           </ScrollView>
           <View style={styles.empowerButton}>
             <Button
@@ -159,8 +217,28 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   empowerContent: {
-    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    backgroundColor: 'white',
     // height: SCREEN_HEIGHT*0.8-80
+  },
+  empowerContentTitle: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginRight: 5,
+  },
+  empowerContentSubTitle: {
+    color: 'black',
+    fontSize: 14,
+    marginTop: 10,
+    marginRight: 5,
+  },
+  empowerContentItem: {
+    color: 'black',
+    fontSize: 12,
+    marginLeft: 10,
+    marginTop: 10,
+    marginRight: 5,
   },
   empowerButton: {
     backgroundColor: 'white',
--- js/env.js
+++ js/env.js
@@ -4,8 +4,8 @@ module.exports = {
   isDebuggingInChrome: true,
   testMenuEnabled: false,
   nodeList: [
+    { url: 'ws://119.23.52.40:11011',     location: 'comm.node.china' },
     { url: 'ws://54.169.30.79:11011', location: 'comm.node.usa' },
-    { url: 'ws://119.23.52.40:11011', 		location: 'comm.node.china' },
     //{ url: 'ws://39.108.54.180:11011',  location: 'comm.node.hongkong' },
     // {url: "ws://119.23.40.206:21011", location: "comm.node.china"},
     // {url: "wss://blockasset.org/ws", 	location: "Newest NODE"},
